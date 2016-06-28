e6c078ac6f9156cb342f5b1aad321d8d4e161d15
Zsolt Fabok <me@zsoltfabok.com>
2016-06-28 12:58:45 +0200

    problem 1

---
+++ src/main/java/project_euler/problems/Problem001.java
@@ -0,0 +1,18 @@
+package project_euler.problems;
+
+import java.util.stream.IntStream;
+
+/**
+ * If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+ *
+ * Find the sum of all the multiples of 3 or 5 below 1000.
+ */
+public class Problem001 {
+    public int solution() {
+        return execute(1000);
+    }
+
+    int execute(int limit) {
+        return IntStream.range(0, limit).filter(n -> n % 3 == 0 || n % 5 == 0).sum();
+    }
+}
---
+++ src/test/java/project_euler/problems/Problem001Test.java
@@ -0,0 +1,31 @@
+package project_euler.problems;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class Problem001Test {
+
+    private Problem001 problem;
+
+    @Before
+    public void setup() {
+        problem = new Problem001();
+    }
+
+    @Test
+    public void returns_23_for_numbers_below_10() {
+        assertEquals(23, problem.execute(10));
+    }
+
+    @Test
+    public void returns_233168_for_numbers_below_1000() {
+        assertEquals(233168, problem.execute(1000));
+    }
+
+    @Test
+    public void solution() {
+        assertEquals(233168, problem.solution());
+    }
+}
